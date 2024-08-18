e0810d0889229f14be67f697de0a0bd78a799e7d
Nhat Pham <turu743@gmail.com>
2024-08-18 22:14:26 +0700

    Chapter2

---
+++ src/components/MyComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class MyComponent extends React.Component{
+
+    render(){
+
+        return(
+            <>
+                <h1>Welcome to my components</h1>
+                {Math.random()}
+
+            </>
+        )
+
+    }
+
+}
+export default MyComponent
