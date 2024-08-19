2173df70a066e8114a3145f81fe1f059cc7b7a98
Nhat Pham <turu743@gmail.com>
2024-08-19 14:49:15 +0700

    setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,12 +3,26 @@ import React from 'react';
 
 class MyComponent extends React.Component{
 
+    state ={
+        name:'nhat',
+        age:12
+
+    }
+    handleSubmit=(e)=>{
+        console.log('this is',this.state.name);
+        
+        this.setState({
+            name:'han'
+        })
+
+    }
+
     render(){
 
         return(
             <>
-                <h1>Welcome to my components</h1>
-                {Math.random()}
+                <h1>{this.state.name}</h1>
+                <button onClick={this.handleSubmit}>Click</button>
 
             </>
         )
