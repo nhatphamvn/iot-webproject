01afb0fa1355caa27a55decb329184ef470c6d67
Nhat Pham <turu743@gmail.com>
2024-09-07 23:19:12 +0700

    Css for Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,28 +2,30 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 const Header=()=> {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Pham Van Nhat</Navbar.Brand>
+        <NavLink to="/" className='navbar-brand'>Pham Van Nhat</NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to="/" className='nav-link'>Home</Link>
-            <Link to="/users" className='nav-link'>User</Link>
-            <Link to="/admin" className='nav-link'>Admin</Link>
+            <NavLink to="/" className='nav-link'>Home</NavLink>
+            <NavLink to="/users" className='nav-link'>User</NavLink>
+            <NavLink to="/admin" className='nav-link'>Admin</NavLink>
           
 
     
           </Nav>
         <Nav>
-            <NavDropdown title="Sign In/Sigh Out" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Sign In</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Sign Out</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">ProFile</NavDropdown.Item>
-            </NavDropdown>
+          <button className='btn-button btn-login'>Log In</button>
+          <button className='btn-button btn-logout'>Sign Up</button>
+            {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
+              <NavDropdown.Item >Sign In</NavDropdown.Item>
+              <NavDropdown.Item >Sign Out</NavDropdown.Item>
+              <NavDropdown.Item >ProFile</NavDropdown.Item>
+            </NavDropdown> */}
             
         </Nav>
         </Navbar.Collapse>
